458a22027a1da5c3c210dd42eef86ec4e42885a8
Bril3d <khaledtbk@gmail.com>
2023-04-05 15:44:30 +0100

    Intial Commit

---
+++ resources/js/Pages/Public/Home.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    props:{
+        users:Object,
+    }
+};
+</script>
+
+<template>
+    <div
+        class="bg-slate-900 flex flex-col items-center justify-center min-h-screen"
+    >
+        <div class="flex flex-column">
+            <div class="p-4" v-for="user in users">
+                {{ user.name }}
+            </div>
+        </div>
+    </div>
+</template>
